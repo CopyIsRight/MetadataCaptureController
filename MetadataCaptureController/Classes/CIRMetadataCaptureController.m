96fc64b61bd5252dbe3b5c7afbf1b8567b1308ae
Pietro Caselani <pietro.caselani@involves.com.br>
2016-06-15 19:21:23 -0300

    Simple implementation and focus view

---
+++ MetadataCaptureController/Classes/CIRMetadataCaptureController.m
@@ -0,0 +1,28 @@
+//
+//  CIRMetadataCaptureController.m
+//  Pods
+//
+//  Created by Pietro Caselani on 15/06/16.
+//
+//
+
+#import "CIRMetadataCaptureController.h"
+#import "MTBBarcodeScanner.h"
+
+@interface CIRMetadataCaptureController ()
+
+@end
+
+@implementation CIRMetadataCaptureController
+
+//region Initialization
+- (void)awakeFromNib
+{
+	_scanner = [[MTBBarcodeScanner alloc] initWithPreviewView:self.view];
+	
+	if (_focusView)
+		_scanner.scanRect = _focusView.frame;
+}
+//endregion
+
+@end
